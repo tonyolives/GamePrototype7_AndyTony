cb57ef84254bfa3d52af0f6d11fc479697e56cb0
Amari M. <100742004+SuperAmarioBros@users.noreply.github.com>
2025-04-15 19:31:11 -0700

    Color Locked Turrets

--- Assets/Scripts/MissileTurretScript.cs
+++ Assets/Scripts/MissileTurretScript.cs
@@ -6,13 +6,34 @@ public class MissileTurretScript : MonoBehaviour
     public GameObject blueMissilePrefab; // reference to blue missile prefab
     public float fireRate = 2f; // time between shots
     public Transform firePoint; // where the missile spawns from
+    private SpriteRenderer _spriteRenderer;
 
     private Transform player;
+
+    public bool isBlue;
+    public bool isRed;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        InvokeRepeating("FireMissile", 1f, fireRate); // start firing repeatedly
+        
+        if ((isBlue == false && isRed == false) || (isBlue == true && isRed == true))
+        {
+            InvokeRepeating("FireMissile", 1f, fireRate); // start firing repeatedly
+        }
+        if (isBlue == false && isRed == true)
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.color = Color.red;            
+            InvokeRepeating("FireRed", 1f, fireRate); // start firing red missiles repeatedly
+        }
+
+        if (isBlue == true && isRed == false)
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.color = Color.blue;            
+            InvokeRepeating("FireBlue", 1f, fireRate); // start firing red missiles repeatedly
+        } 
     }
 
     void Update()
@@ -29,6 +50,21 @@ public class MissileTurretScript : MonoBehaviour
     {
         // randomly choose a red or blue missile to fire
         GameObject prefab = (Random.value > 0.5f) ? redMissilePrefab : blueMissilePrefab;
+
+        Instantiate(prefab, firePoint.position, firePoint.rotation);
+    }
+
+    void FireRed()
+    {
+        // Choose red missile and fire
+        GameObject prefab = redMissilePrefab;
+        Instantiate(prefab, firePoint.position, firePoint.rotation);
+    }
+
+    void FireBlue()
+    {
+        // Choose blue missile and fire
+        GameObject prefab = blueMissilePrefab;
         Instantiate(prefab, firePoint.position, firePoint.rotation);
     }
 }
