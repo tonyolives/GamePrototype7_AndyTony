17268cd4bc50a9423b4c96ecfc424bff7bf154ec
Rachel Brockman <rachelb7@usc.edu>
2025-04-28 22:46:46 -0700

    Fixed lvl2 look and working on music

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -185,6 +185,9 @@ public class PlayerScript : MonoBehaviour
 
     private IEnumerator FlashGreenAndRestart()
     {
+        //Stop bkrd audio
+        bkrd._audio.Stop();
+
         // Set dying flag to true to stop movement during victory animation
         isDying = true;
 
